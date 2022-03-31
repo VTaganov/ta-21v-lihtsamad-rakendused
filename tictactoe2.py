ebd522c08ad74af01801126194c7f8a047e816f4
VTaganov <vladimir.taganov@tptlive.ee>
2022-03-31 11:07:21 +0300

    546

--- tictactoe2.py
+++ tictactoe2.py
@@ -11,4 +11,8 @@ print(game[6] + game[7] + game[8])
 next_position = input("Please type what position: ")
 
 if next_position == "1":
-    game
+    game[0]="X"
+
+print(game[0] + game[1] + game[2])
+print(game[3] + game[4] + game[5])
+print(game[6] + game[7] + game[8])
